8c82ddd5e773ab19d9597e61b985f4d598b5752b
Nikolai Borisik <m.barysik@sam-solutions.com>
2021-02-20 19:27:06 +0300

    Exercises 1, 2

--- cypress/integration/02-adding-items/spec.js
+++ cypress/integration/02-adding-items/spec.js
@@ -1,24 +1,39 @@
 /// <reference types="cypress" />
 it('loads', () => {
   // application should be running at port 3000
-  cy.visit('localhost:3000')
+
   cy.contains('h1', 'todos')
 })
 
+beforeEach(function resetData() {
+  cy.request('POST', '/reset', {
+    todos: []
+  })
+  cy.visit('/')
+})
 // IMPORTANT ⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️
 // remember to manually delete all items before running the test
 // IMPORTANT ⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️
+const addNewItem = item => cy.get('.new-todo').type(`${item}{enter}`)
 
 it('adds two items', () => {
-  // repeat twice
-  //    get the input field
-  //    type text and "enter"
-  //    assert that the new Todo item
-  //    has been added added to the list
+  addNewItem('todo 1')
+  addNewItem('todo 2')
   // cy.get(...).should('have.length', 2)
+  cy.get('.todo').should('have.length', 2)
 })
 
 it('can mark an item as completed', () => {
+  addNewItem('item 1')
+  addNewItem('item 2')
+  cy.get('.todo ')
+    .first()
+    .find('[type=checkbox]')
+    .click()
+  cy.get('.todo')
+    .first()
+    .should('have.class', 'completed')
+  cy.get('.todo.completed').should('have.length', 1)
   // adds a few items
   // marks the first item as completed
   // confirms the first item has the expected completed class
@@ -26,43 +41,44 @@ it('can mark an item as completed', () => {
 })
 
 it('can delete an item', () => {
-  // adds a few items
-  // deletes the first item
-  // use force: true because we don't want to hover
-  // confirm the deleted item is gone from the dom
-  // confirm the other item still exists
+  addNewItem('item 1')
+  addNewItem('item 2')
+  cy.get('.todo')
+    .first()
+    .find('.destroy')
+    .click({ force: true })
+  cy.get('.todo').should('have.length', 1)
 })
 
 it('can add many items', () => {
   const N = 5
   for (let k = 0; k < N; k += 1) {
-    // add an item
-    // probably want to have a reusable function to add an item!
+    addNewItem(`itemt ${k}`)
   }
-  // check number of items
+  cy.get('.todo').should('have.length', N)
 })
 
 it('adds item with random text', () => {
-  // use a helper function with Math.random()
-  // or Cypress._.random() to generate unique text label
-  // add such item
-  // and make sure it is visible and does not have class "completed"
+  const label = `New Item ${Math.random()}`
+  addNewItem(label)
+  cy.contains('.todo', label)
+    .should('not.have.class', 'completed')
+    .and('be.visible')
 })
 
 it('starts with zero items', () => {
-  // check if the list is empty initially
-  //   find the selector for the individual TODO items
-  //   in the list
-  //   use cy.get(...) and it should have length of 0
-  //   https://on.cypress.io/get
+  cy.get('.todo').should('have.length', 0)
 })
 
 it('does not allow adding blank todos', () => {
   // https://on.cypress.io/catalog-of-events#App-Events
-  cy.on('uncaught:exception', () => {
-    // check e.message to match expected error text
-    // return false if you want to ignore the error
+
+  cy.on('uncaught:exception', (e) => {
+
+    return  !e.message.includes('Cannot add a blank todo')
   })
+
+  addNewItem('a')
 
   // try adding an item with just spaces
 })
